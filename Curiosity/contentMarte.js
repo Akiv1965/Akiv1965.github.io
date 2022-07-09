51081e708950a27cee098ebfcb5d3e6a87b6692a
Akiv Yo <Akiv1965>
2022-07-09 03:58:21 +0200

    1.5

--- Curiosity/contentMarte.js
+++ Curiosity/contentMarte.js
@@ -6,16 +6,7 @@
 		idioma = "en"
 	}
 
- function curiosityLaunch () {
-		// Abre página de créditos
-		// Open credit page
-		$(".linkTitulo").on("click", function () {
-			chrome.windows.create({ url: "creditos/creditos.html", width: 330, height: 650, left: 250, top: 12, type: "popup" })
-		})
-
-		$("#starGif").on("click", function () {
-			window.open("https://play.google.com/store/apps/details?id=com.rover.curiositycams", "_blank")
-		})
+function curiosityLaunch () {
 
 		//Conexión AJAX con api.nasa 
 		$.ajax({
@@ -25,14 +16,14 @@
 			type: "GET",
 			url: "https://api.nasa.gov/mars-photos/api/v1/manifests/curiosity?api_key=hb4mXofbbZ3GHnWy48wjFiXBGj7wkadXhEN9TOYA",
 			success: function (data) {
-				console.log(data.photo_manifest)
+				console.log(data)
 				var fotoLength = data.photo_manifest.photos.length - 1;
 				var fotoLengthArchivo = fotoLength
 				var diaSolUltimo = data.photo_manifest.photos[fotoLength].sol
 				var diaSolUltimoArchivo
 				var diaTierraUltimo=new Date(data.photo_manifest.photos[fotoLengthArchivo].earth_date).toLocaleDateString()
 				var arrayImagenes = []
-				$("#cabListaFechas").html("<small>"+diaTierraUltimo+" - <small>(Sol"+diaSolUltimo+")</small></small><hr>Cámaras On-Line: <big>"+data.photo_manifest.photos[fotoLengthArchivo].cameras.length+"</big> <small> ("+data.photo_manifest.photos[fotoLengthArchivo].total_photos+" Fotos)</small>")
+				$("#cabListaFechas").html("<small>"+diaTierraUltimo+" - <small>(Sol"+diaSolUltimo+")</small></small><br><span style='color: rgb(173, 255, 47)'>Cámaras On-Line: <big>"+data.photo_manifest.photos[fotoLengthArchivo].cameras.length+"</big> <small> ("+data.photo_manifest.photos[fotoLengthArchivo].total_photos+" Fotos)</small></span>")
 				//Monta desplegable para archivo de fechas
 				//Set menu for dates archive	
 				for (var k = 0; k < 8; k++) {
@@ -58,7 +49,7 @@
 					camaraArray = data.photo_manifest.photos[fotoLengthArchivo].cameras
 					diaSolUltimo = data.photo_manifest.photos[fotoLengthArchivo].sol
 					camara = data.photo_manifest.photos[fotoLengthArchivo].cameras[0]
-					$("#cabListaFechas").html("<small>"+data.photo_manifest.photos[fotoLengthArchivo].earth_date+" - <small>(Sol"+diaSolUltimo+")</small></small><hr> Cámaras On-Line: <big>"+data.photo_manifest.photos[fotoLengthArchivo].cameras.length+"</big><small> ("+data.photo_manifest.photos[fotoLengthArchivo].total_photos+" Fotos)</small>")
+					$("#cabListaFechas").html("<small>"+data.photo_manifest.photos[fotoLengthArchivo].earth_date+" - <small>(Sol"+diaSolUltimo+")</small></small><br><span style='color: rgb(173, 255, 47)'>Cámaras On-Line: <big>"+data.photo_manifest.photos[fotoLengthArchivo].cameras.length+"</big><small> ("+data.photo_manifest.photos[fotoLengthArchivo].total_photos+" Fotos)</small></span>")
 					creaBotones()
 					setFoto()
 				})
@@ -66,14 +57,12 @@
 
 				// Función para cambiar de cámaras
 				// Function for change cams
-				$("#cajaBotones").on("click", function (e) {
+				$("#cajaBotones .botonCamara").on("click", function (e) {
 					console.log($(this).attr("id"))
 					console.log(e.target.id)
 					camara = e.target.id//$(this).attr("id")
-					//$(".botonCamara").css({ "color": "rgb(173, 255, 47, 0.8)", "font-weight": "normal" })
-					$("button:enabled").css({ "color": "rgb(170, 255, 40, 0.8)", "font-weight": "bold" })
-					$("#" + camara).css({ "color": "rgba(200, 40, 40)", "font-weight": "bold" })
 					indexFoto = 0;
+					creaBotones(camara)
 					setFoto();
 				})
 
@@ -83,12 +72,23 @@
 					if ($(this).attr("id") == "next") {
 						indexFoto++;
 						console.log(indexFoto)
+						scrollX = scrollX + 50
+						if (scrollX > 150) {
+							scrollWin(scrollX)
+						}
 						setFoto();
 					} else {
 						indexFoto--;
+
+						scrollX = scrollX - 50
+						if (scrollX < 150) {
+						scrollWin(scrollX)
+						}
 						if (indexFoto < 0) {
 							indexFoto = 0
-							window.alert("Está al principio de la galería")
+							scrollX = 0
+							scrollWin(scrollX)
+							window.alert("Está al principio de la galería")							
 						}
 						setFoto()
 					}
@@ -99,20 +99,22 @@
 				$(".row").click(function (e) {
 					// console.log(e)
 					console.log(e.originalEvent.path)
-					$("#galeria #" + ($("#primer").val() - 1) + " #indexThumb").css({ width: "19px", fontSize: "0.9em" })
+					
+					$("span #indexThumb").css({"width": "20px", "fontSize": "0.9em","color": "rgb(173, 255, 47, 0.9)" })
 					var urlImg = e.originalEvent.path[0].currentSrc
 					indexFoto = Number(e.originalEvent.path[1].id) + 1
 					$("#primer").val(indexFoto)
 					$("#imgMarte").attr("src", urlImg)
-					//$("#galeria #"+($("#primer").val()-1)+" #indexThumb").animate({width: "45px", fontSize: "1.4em"})
+					$("#galeria #"+(indexFoto-1)+" #indexThumb").animate({width: "45px", fontSize: "1.4em"})
 					indexFoto--;
 				})
 
 				// Crea botones para cámaras con imágenes
 				// Set buttons for cams with images
-				function creaBotones(nombreCamaraFull) {
+				function creaBotones(camara) {
 					//$("#cajaBotones").empty()
 					$(":button").css({"color": "rgb(15, 205, 30,0.6)", "opacity": "0.4"})
+					$(".nombreCamOculto").css({"color": "rgb(15, 205, 30,0.6)", "opacity": "0.4"})
 					$(":button").attr("disabled", true)
 					indexFoto = 0
 					for (var i = 0; i < camaraArray.length; i++) {
@@ -121,40 +123,64 @@
 						if (idioma.slice(0, 2) == "es") {
 							switch (camaraArray[i]) {
 								case "FHAZ":
-									$("#" + camaraArray[i]).css({ "color": "rgba(170, 255, 40)", "font-weight": "bold", "opacity": "1" })
+									$("#" + camaraArray[i]).css({ "color": "rgba(170, 255, 40)", "opacity": "1", "box-shadow": "0px 0px 4px rgba(170, 255, 40)" })
+									$("#" + camaraArray[i]+"nombre").css({ "color": "rgba(170, 255, 40)", "opacity": "1" })
 									$("#"+camaraArray[i]).attr("disabled", false)
 									break;
 								case "RHAZ":
-									$("#" + camaraArray[i]).css({ "color": "rgba(170, 255, 40)", "font-weight": "bold", "opacity": "1" })
+									$("#" + camaraArray[i]).css({ "color": "rgba(170, 255, 40)", "opacity": "1", "box-shadow": "0px 0px 4px rgba(170, 255, 40)" })
+									$("#" + camaraArray[i]+"nombre").css({ "color": "rgba(170, 255, 40)", "opacity": "1" })
 									$("#"+camaraArray[i]).attr("disabled", false)
 									break;
 								case "MAST":
-									$("#" + camaraArray[i]).css({ "color": "rgba(170, 255, 40)", "font-weight": "bold", "opacity": "1" })
+									$("#" + camaraArray[i]).css({ "color": "rgba(170, 255, 40)", "opacity": "1", "box-shadow": "0px 0px 4px rgba(170, 255, 40)" })
+									$("#" + camaraArray[i]+"nombre").css({ "color": "rgba(170, 255, 40)", "opacity": "1"})
 									$("#"+camaraArray[i]).attr("disabled", false)
 									break;
 								case "CHEMCAM":
-									$("#" + camaraArray[i]).css({ "color": "rgba(170, 255, 40)", "font-weight": "bold", "opacity": "1" })
+									$("#" + camaraArray[i]).css({ "color": "rgba(170, 255, 40)", "opacity": "1", "box-shadow": "0px 0px 4px rgba(170, 255, 40)" })
+									$("#" + camaraArray[i]+"nombre").css({ "color": "rgba(170, 255, 40)","opacity": "1" })
 									$("#"+camaraArray[i]).attr("disabled", false)
 									break;
 								case "MAHLI":
-									$("#" + camaraArray[i]).css({ "color": "rgba(170, 255, 40)", "font-weight": "bold", "opacity": "1" })
+									$("#" + camaraArray[i]).css({ "color": "rgba(170, 255, 40)", "opacity": "1", "box-shadow": "0px 0px 4px rgba(170, 255, 40)" })
+									$("#" + camaraArray[i]+"nombre").css({ "color": "rgba(170, 255, 40)", "opacity": "1" })
 									$("#"+camaraArray[i]).attr("disabled", false)
 									break;
 								case "MARDI":
-									$("#" + camaraArray[i]).css({ "color": "rgba(170, 255, 40)", "font-weight": "bold", "opacity": "1" })
+									$("#" + camaraArray[i]).css({ "color": "rgba(170, 255, 40)", "opacity": "1", "box-shadow": "0px 0px 4px rgba(170, 255, 40)" })
+									$("#" + camaraArray[i]+"nombre").css({ "color": "rgba(170, 255, 40)", "opacity": "1" })
 									$("#"+camaraArray[i]).attr("disabled", false)
 									break;
 								case "NAVCAM":
-									$("#" + camaraArray[i]).css({ "color": "rgba(170, 255, 40)", "font-weight": "bold", "opacity": "1" })
+									$("#" + camaraArray[i]).css({ "color": "rgba(170, 255, 40)", "opacity": "1", "box-shadow": "0px 0px 4px rgba(170, 255, 40)" })
+									$("#" + camaraArray[i]+"nombre").css({ "color": "rgba(170, 255, 40)", "opacity": "1"})
 									$("#"+camaraArray[i]).attr("disabled", false)
 									break;
 							}
 							console.log(camaraArray[0])
-						$("#" + camaraArray[0]).css({ "color": "rgba(200, 40, 40)", "font-weight": "bold", "opacity": "1" })
-						}
-						
+						$("#" + camaraArray[0]).css({ "color": "rgba(250, 40, 40)", "opacity": "1", "box-shadow": "0px 0px 4px rgba(200, 40, 40)" })
+						$("#" + camaraArray[0]+"nombre").css({ "color": "rgba(200, 40, 40)", "opacity": "1" })
 					}
-					$("#cajaBotones").append("<span id='ping' hidden><img id='imgBall' src='iconos/pingGreen.png' width='9px'></span>")
+					var camaraTemp= camaraArray[0]
+					$("#datos").text(camaraTemp)	
+					}
+					
+					if(camara !== undefined){
+						$("#" + camaraTemp).css({ "color": "rgba(170, 255, 40)", "opacity": "1", "box-shadow": "0px 0px 4px rgba(170, 255, 40)" })
+						$("#" + camaraTemp+"nombre").css({ "color": "rgba(170, 255, 40)", "opacity": "1" })
+						$("#"+camaraTemp).attr("disabled", false)
+						$("#" + camara).css({ "color": "rgba(250, 40, 40)", "opacity": "1", "box-shadow": "0px 0px 4px rgba(210, 40, 40)" })
+						$("#" + camara+"nombre").css({ "color": "rgba(200, 40, 40)", "opacity": "1" })
+						$("#datos").text(camara)
+					}
+					$("button:enabled").css("cursor", "pointer")
+					//$("#cajaBotones").append("<span id='ping' hidden><img id='imgBall' src='iconos/pingGreen.png' width='9px'></span>")
+				}
+
+				// Función SCROLL  ****
+				function scrollWin(scrollX) {
+					document.getElementById('galeria').scrollTo(scrollX, 0);
 				}
 
 				// Función para montar la galería
@@ -170,69 +196,56 @@
 							indexFoto--;
 							window.alert("Esta cámara no tiene más imágenes")
 						}
+
 						var fechaImg = new Date(fotoMarte[indexFoto].earth_date).toLocaleDateString()
 						var nombreCamaraFull = fotoMarte[indexFoto].camera.full_name
-						if (idioma.slice(0, 2) == "es") {
-							switch (fotoMarte[indexFoto].camera.name) {
-								case "FHAZ":
-									nombreCamaraFull = "Cámara frontal para prevención de riesgos"
-									break;
-								case "RHAZ":
-									nombreCamaraFull = "Cámara trasera para prevención de riesgos"
-									break;
-								case "MAST":
-									nombreCamaraFull = "Cámara del mástil"
-									break;
-								case "CHEMCAM":
-									nombreCamaraFull = "Conjunto de cámaras para química"
-									break;
-								case "MAHLI":
-									nombreCamaraFull = "Cámara manual"
-									break;
-								case "MARDI":
-									nombreCamaraFull = "Cámara de descenso"
-									break;
-								case "NAVCAM":
-									nombreCamaraFull = "Cámara de navegación"
-									break;
-							}
-						}
-						$("#nombreCam").text(nombreCamaraFull + " (" + fotoMarte[indexFoto].camera.name + ")")
 						$("#primer").val(indexFoto + 1)
 						$("#ultimo").val(data.photos.length)
 						$("#ultimo").attr("max", data.photos.length)
 						ultimoIndexReset = data.photos.length
 						$(".slides").html("<img src='" + fotoMarte[indexFoto].img_src + "' style='width: 100%;' id='imgMarte'> ")
+						
 						for (var k = 0; k <= fotoMarte.length - 1; k++) {
 							arrayImagenes.push(fotoMarte[k].img_src)
 							$(".row").append("<span class='column' id='" + k + "'><span id='indexThumb'>" + (k + 1) + "</span><img  src='" + fotoMarte[k].img_src + "' style='width:100%; height:100%'></span>")
 						}
+						$("#galeria #" + indexFoto+ " #indexThumb").animate({ fontSize: "1.4em", color: "red" }, 300)
 						//$("#galeria #"+($("#primer").val()-1)+" #indexThumb").animate({width: "45px", fontSize: "1.4em"})	
 					})
 				}//Fin función setFoto
+
+				
 
 				var intervalImg;
 				var indexArrayImg//=Number($("#primer").val())-1
 				var ultimoIndex//= Number($("#ultimo").val())
 				var primerIndex;
 				var scrollX = -50
-				// Función SCROLL  ****
-				function scrollWin(scrollX) {
-					document.getElementById('galeria').scrollTo(scrollX, 0);
+				
+				function eventoClick() {
+					primerIndex = Number($("#primer").val()) - 1
+					indexArrayImg = Number($("#primer").val()) - 1
+					ultimoIndex = Number($("#ultimo").val())
+					intervalImg = setInterval(moverImagenes, 500)
+					$("#galeria #" + indexFoto + " #indexThumb").animate({ width: "19px", fontSize: "0.9em" })
 				}
+
 
 
 				function moverImagenes() {
 					// $("#slidesMovie").show()
 					//$("#slidesMovie").css({"display": "flex"})
-					$("#fechasID").hide()
-					$(".botonCamara").hide()
-					$(".flechas").hide()
-					$(".numbertext").hide()
-					$("#botonMovie").hide()
+					$("#fechasID").attr("disabled", "true")
+					$(".botonCamara").css({"opacity": "0.1"})
+					$(".nombreCamOculto").css({"opacity": "0.1"})
+					$("#"+$("#datos").text()).css({"opacity": "1"})
+					$("#"+$("#datos").text()+"nombre").css({"opacity": "1"})
+					//$(".flechas").hide()
+					//$(".numbertext").hide()
+					$("#botonMovie").off("click")
 					$("#target").hide()
 					$("#ping").show()
-
+					console.log($("#datos").text(), "....", ".botonCamara #"+$("#datos").text())
 					// $("#img"+indexArrayImg).css({"position": "sticky", "left": "0px"})
 
 					$("#primer").val(indexArrayImg + 1)
@@ -240,7 +253,8 @@
 					
 					$("#galeria #" + indexArrayImg).animate({ fontSize: "1.4em", color: "red" }, 300)
 					$("#galeria #" + indexArrayImg).animate({ fontSize: "0.9em", color: "rgb(173, 255, 47, 0.9)" }, 500)
-
+				
+				//Control movimiento scroll
 					scrollX = scrollX + 50
 					if (indexArrayImg == 0) {
 						scrollX = 0
@@ -260,16 +274,13 @@
 					}
 					$(".slides").html("<img src='" + arrayImagenes[indexArrayImg] + "' style='width: 100%' id='imgMarte'> ")
 				}
-				$("#botonMovie").on("click", function () {
-					$("#botonMovie").hide()
-					$("#botonCerrarMovie").show()
-					primerIndex = Number($("#primer").val()) - 1
-					indexArrayImg = Number($("#primer").val()) - 1
-					ultimoIndex = Number($("#ultimo").val())
-					intervalImg = setInterval(moverImagenes, 500)
-					$("#galeria #" + indexFoto + " #indexThumb").animate({ width: "19px", fontSize: "0.9em" })
-				})
+			//EVENTO para PLAY MOVIE
+				$("#botonMovie").on("click", eventoClick)
+
+			//EVENTO para PARAR MOVIE
 				$("#botonCerrarMovie").on("click", function () {
+					
+					$("#botonMovie").on("click", eventoClick)
 
 					clearInterval(intervalImg)
 					indexFoto = Number($("#primer").val()) - 1
@@ -277,11 +288,10 @@
 					scrollX = scrollX - 50
 					$("#ping").hide();
 					$("#fechasID").show()
-					$("#botonCerrarMovie").hide()
-					$(".botonCamara").show()
+					$(".botonCamara").css({"opacity": "1"})
+					$(".nombreCamOculto").css({"opacity": "1"})
 					$(".flechas").show()
 					$(".numbertext").show()
-					$("#botonMovie").show()
 					$("#target").show()
 					$("#galeria #" + indexFoto + " #indexThumb").animate({ width: "45px", fontSize: "1.4em" })
 					$("#ultimo").val(ultimoIndexReset)
@@ -294,7 +304,7 @@
 		// Abre ventana pata imagen original
 		// Open window for original image
 		$("#target").on("click", function () {
-			chrome.windows.create({ url: $("#imgMarte").attr("src"), width: 700, left: 50, top: 9, type: "popup" })
+			window.open($('#imgMarte').attr('src'), "_blank", "toolbar=no,scrollbars=yes,menubar=no,resizable=yes,top=50,left=400,width=550,height=450")
 		})
 
 		// // Conexión AJAX para coger datos clima
